ac63a63a900e11874044153615508fa5da668a09
nyllewin <stxlez@gmail.com>
2017-12-12 08:55:44 +0100

    update offsets for 2.6.1.48432

--- Enigma.D3/Enigma.D3.Memory.Generated/ObjectPtr.cs
+++ Enigma.D3/Enigma.D3.Memory.Generated/ObjectPtr.cs
@@ -2,23 +2,23 @@ namespace Enigma.D3.Memory
 {
     public static class ObjectPtr
     {
-        public const int VideoPreferences = 0x1E967F0;
-        public const int SoundPreferences = 0x1E96890;
-        public const int HotkeyPreferences = 0x1E968E0;
-        public const int GameplayPreferences = 0x1E96D60;
-        public const int SocialPreferences = 0x1E96DC8;
-        public const int ChatPreferences = 0x1E96DF8;
-        public const int LevelArea = 0x207A7C0;
-        public const int LevelAreaName = 0x207A7C8;
-        public const int MapActId = 0x207AAB0;
-        public const int SNOGroupsByCode = 0x20EBE28;
-        public const int SNOGroups = 0x20EBE24;
-        public const int ObjectManager = 0x20EC070;
-        public const int ObjectManagerPristine = 0x20EC060;
-        public const int ApplicationLoopCount = 0x20EC0EC;
-        public const int LocalData = 0x20ED320;
-        public const int AttributeDescriptors = 0x21443D0;
-        public const int ContainerManager = 0x21B6E64;
-        public const int MessageDescriptor = 0x21B6E70;
+        public const int VideoPreferences = 0x1e96800;
+        public const int SoundPreferences = 0x1e968a0;
+        public const int HotkeyPreferences = 0x1e968f0;
+        public const int GameplayPreferences = 0x1e96d70;
+        public const int SocialPreferences = 0x1e96dd8;
+        public const int ChatPreferences = 0x1e96e08;
+        public const int LevelArea = 0x207a7d0;
+        public const int LevelAreaName = 0x207a7d8;
+        public const int MapActId = 0x207aac0;
+        public const int SNOGroupsByCode = 0x20ebe38;
+        public const int SNOGroups = 0x20ebe34;
+        public const int ObjectManager = 0x20ec080;
+        public const int ObjectManagerPristine = 0x20ec070;
+        public const int ApplicationLoopCount = 0x20ec0fc;
+        public const int LocalData = 0x20ed330;
+        public const int AttributeDescriptors = 0x21443e0;
+        public const int ContainerManager = 0x21b6e74;
+        public const int MessageDescriptor = 0x21b6e80;
     }
 }
